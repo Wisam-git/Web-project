71ab23c97ed232e09a0588f8e0137bad5bafb3c2
Wisam-git <92330233@students.liu.edu.lb>
2026-06-02 09:12:47 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import Header from './Components/Header.js';
 import Home from './Pages/Home.js';
 import Employees from './Pages/Employees.js';
 import Animals from './Pages/Animals.js';
-import Test from './Pages/Test.js';
+
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/Employees' element={<Employees />} />
           <Route path='/Animals' element={<Animals />} />
-          <Route path='/Test' element={<Test />} />
+     
         </Routes>
       </BrowserRouter>
     </div>
